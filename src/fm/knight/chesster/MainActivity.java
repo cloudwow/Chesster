be6bf95d5e0bb52a3a3b306efdac768a3df181e5
David Knight <david@Cats-Down-Under-the-Stars.local>
2013-11-04 05:59:09 -0800

    initial commit

---
+++ src/fm/knight/chesster/MainActivity.java
@@ -0,0 +1,16 @@
+package fm.knight.chesster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //        setContentView(R.layout.main);
+        setContentView(new BoardView(this));
+    }
+}
