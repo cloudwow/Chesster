801b4d088c11bb4dc71c9d2748b9de2a7ee2ceee
David Knight <david@Cats-Down-Under-the-Stars.local>
2013-11-09 14:10:26 -0800

    ui

--- android/src/fm/knight/chesster/view/BoardView.java
+++ android/src/fm/knight/chesster/view/BoardView.java
@@ -2,8 +2,11 @@ package fm.knight.chesster.view;
 
 import fm.knight.chesster.model.piece.Piece;
 import fm.knight.chesster.model.Board;
+import fm.knight.chesster.model.Coordinate;
+import fm.knight.chesster.R;
 
 import android.content.Context;
+import android.util.Log;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,12 +15,23 @@ import android.graphics.RectF;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.os.*;
+import android.view.*;
 
 public class BoardView extends View {
 
+  private static final String TAG = "Chesster.BoardView";
   private final Board board;
-  int boardSize;
-  int squareSide;
+  int mViewWidth;
+  int mViewHeight;
+  int mPosX[] = new int[8];
+  int mPosY[] = new int[8];
+  int mTileSize;
+  Coordinate focusCoordinate;
   public BoardView (Context context, Board board) {
     super(context);
     setFocusable(true);
@@ -64,56 +78,80 @@ public class BoardView extends View {
     return true;
   }
 
+
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    mViewWidth = w;
+    mViewHeight = h;
+    mTileSize = Math.min(mViewWidth, mViewHeight) / 8;
+    for (int i = 0; i < 8; i++) {
+      mPosX[i] =  i * mTileSize;
+      mPosY[i] = (mTileSize * 7) - i * mTileSize;
+    }
+
+  }
+
+
   @Override
   protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-    boardSize = Math.min(canvas.getWidth(), canvas.getHeight());
-    squareSide = boardSize / 8;
-    drawBoard(canvas);
-    //    drawPiece(canvas, piece);
-  }
-
-
-  @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-    int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
-
-    int widthMeasure = MeasureSpec.getSize(widthMeasureSpec);
-    int heightMeasure = MeasureSpec.getSize(heightMeasureSpec);
-
-    int newWidth, newHeight;
-
-    if (widthSpecMode == MeasureSpec.AT_MOST)
-      newWidth = 30;
-    else
-      newWidth = widthMeasure;
-
-    if (heightSpecMode == MeasureSpec.AT_MOST)
-      newHeight = 30;
-    else
-      newHeight = heightMeasure;
-
-    setMeasuredDimension(newWidth, newHeight);
-  }
-
-
-  
-  public void drawBoard(Canvas c) {
-    Paint paint = new Paint();
-    boolean isSet =false;
-    for (int i = 0; i < boardSize; i += squareSide) {
-      isSet = !isSet;
-      for (int j = 0; j < boardSize; j += squareSide) {
-        paint.setColor(isSet ? Color.BLACK : Color.WHITE);
-        c.drawRect(new Rect(i, j, i + squareSide, j + squareSide), paint);
-        isSet = !isSet;
+    Resources r = getResources();
+    BitmapDrawable tile_white_drawable =
+      (BitmapDrawable) r.getDrawable(R.drawable.tile_white);
+    Bitmap tilewhite = tile_white_drawable.getBitmap();
+    BitmapDrawable tileDarkDrawable = (BitmapDrawable) r.getDrawable(R.drawable.tile_dark);
+    Bitmap tileDark = tileDarkDrawable.getBitmap();
+    canvas.drawColor(Color.BLACK);
+    Rect drawRec = new Rect();
+    for (int x = 0; x < 8; x++) {
+      for (int y = 0; y < 8; y++) {
+        drawRec.set(mPosX[x], mPosY[y], mPosX[x] + mTileSize, mPosY[y] + mTileSize);
+        if ((x + y) % 2 == 0)
+          canvas.drawBitmap(tilewhite, null, drawRec, null);
+        else
+          canvas.drawBitmap(tileDark, null, drawRec, null);
       }
     }
+
+    /////////////////// pieces
+    Rect pieceSquare = new Rect();
+    RectF focusSquare = new RectF();
+    Bitmap pieceBitmap;
+    for (int row = 0; row < 8; row++) {
+      for (int column = 0; column < 8; column++) {
+        if (!board.isEmptyAt(row,column)) {
+          Piece piece=board.getPieceAt(row,column);
+          pieceBitmap = BitmapFactory.decodeResource(
+                                                     getResources(),
+                                                     PieceView.getBitmapCode(piece));
+          if(pieceBitmap==null) {
+            Log.w(TAG, "piece bitmap is null");
+          } else {
+            pieceSquare.set(
+                            mPosX[column],
+                            mPosY[row], mPosX[column] + mTileSize,
+                            mPosY[row] + mTileSize);
+            canvas.drawBitmap(pieceBitmap, null, pieceSquare, null);
+          }
+        }
+      }
+    }
+
+    if (focusCoordinate != null){
+      Paint paint = new Paint();
+      paint.setStrokeWidth(4);
+      paint.setStyle(Paint.Style.STROKE);
+      focusSquare.set(mPosX[focusCoordinate.getColumn()]+1,
+          mPosY[focusCoordinate.getRow()]+1,
+          mPosX[focusCoordinate.getRow()]-1 + mTileSize,
+          mPosY[focusCoordinate.getRow()]-1 + mTileSize);
+      paint.setColor(0x400000ff);
+      canvas.drawRoundRect(focusSquare,3F,3F, paint);
+
+    }
+
   }
 
-
-  public void drawPiece(Canvas c, Piece p) {
+  public void setFocus(Coordinate focusCoordinate) {
+    this.focusCoordinate = focusCoordinate;
   }
-
 }
--- android/src/fm/knight/chesster/view/PieceView.java
+++ android/src/fm/knight/chesster/view/PieceView.java
@@ -2,6 +2,12 @@ package fm.knight.chesster.view;
 import fm.knight.chesster.R;
 
 import fm.knight.chesster.model.piece.Piece;
+import fm.knight.chesster.model.piece.Pawn;
+import fm.knight.chesster.model.piece.Knight;
+import fm.knight.chesster.model.piece.Rook;
+import fm.knight.chesster.model.piece.Bishop;
+import fm.knight.chesster.model.piece.Queen;
+import fm.knight.chesster.model.piece.King;
 import android.graphics.Bitmap;
 
 public enum PieceView {
@@ -12,22 +18,45 @@ public enum PieceView {
   QUEEN(R.drawable.chess_piece_black_queen, R.drawable.chess_piece_white_queen, "Q"),
   KING(R.drawable.chess_piece_black_king, R.drawable.chess_piece_white_king, "K");
 
-  private final long whiteResourceId;
-  private final long blackResourceId;
+  private final int whiteResourceId;
+  private final int blackResourceId;
   private final String code;
 
-  PieceView(long blackResourceId, long whiteResourceId, String code) {
+  PieceView(int blackResourceId, int whiteResourceId, String code) {
     this.blackResourceId=blackResourceId;
     this.whiteResourceId=whiteResourceId;
     this.code = code;
   }
 
-  Bitmap getBitmap() {
-    return null;
-  }
 
   String getCode() {
     return code;
   }
 
+  public static int getBitmapCode(Piece piece) {
+    PieceView pieceView;
+    if (piece instanceof Pawn) {
+      pieceView = PAWN;
+    } else if (piece instanceof Rook) {
+      pieceView = ROOK;
+    } else if (piece instanceof Knight) {
+      pieceView = KNIGHT;
+    } else if (piece instanceof Bishop) {
+      pieceView = BISHOP;
+    } else if (piece instanceof Queen) {
+      pieceView = QUEEN;
+    } else if (piece instanceof King) {
+      pieceView = KING;
+    } else {
+      throw new RuntimeException();
+    }
+    switch(piece.getColor()) {
+    case WHITE:
+      return pieceView.whiteResourceId;
+    case BLACK:
+      return pieceView.blackResourceId;
+    default:
+      throw new RuntimeException();
+    }
+  }
 }
